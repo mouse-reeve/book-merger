610c528de98a5307436d1d310f6fa2483bcf8d98
Mouse Reeve <mouse.reeve@gmail.com>
2015-05-23 20:24:27 -0700

    Improves librarything scraped data mappings

--- src/main/java/com/bookmerger/LTScrapedMapper.java
+++ src/main/java/com/bookmerger/LTScrapedMapper.java
@@ -18,24 +18,20 @@ public class LTScrapedMapper extends Mapper<Object, Text, Text, BookMapWritable>
         JsonNode isbn10 = result.get("isbn");
         isbn = new Text(Utilities.normalizeISBN(isbn10.asText()));
 
-        JsonNode places = result.get("places");
-        if (places != null) {
-            data.put(Header.PLACES.asText(), new Text(places.toString()));
-        }
+        String[] fields = {"places", "characters", "events", "year"};
 
-        JsonNode characters = result.get("characters");
-        if (characters != null) {
-        data.put(Header.CHARACTERS.asText(), new Text(characters.toString()));
-        }
+        for (String field : fields) {
+            Header header;
+            try {
+                header = Header.valueOf(field.toUpperCase());
+            } catch (IllegalArgumentException e) {
+                continue;
+            }
+            JsonNode fieldValue = result.get(field);
 
-        JsonNode events = result.get("events");
-        if (events != null) {
-            data.put(Header.EVENTS.asText(), new Text(events.toString()));
-        }
-
-        JsonNode year = result.get("year");
-        if (year != null) {
-            data.put(Header.DATE_PUBLISHED.asText(), new Text(year.toString()));
+            if (fieldValue != null) {
+                data.put(header.asText(), new Text(fieldValue.toString()));
+            }
         }
 
         if (isbn != null) {
